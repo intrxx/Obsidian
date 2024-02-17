bd77808a3c01658697345eabfc556e48043cfaa7
Michał Ogiński <michaloginski15@gmail.com>
2024-02-17 19:24:16 +0100

    Added HeroComponent that will include PlayerInput setup

--- Source/Obsidian/Obsidian.Build.cs
+++ Source/Obsidian/Obsidian.Build.cs
@@ -8,9 +8,21 @@ public class Obsidian : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] 
+			{ 
+				"Core", 
+				"CoreUObject", 
+				"Engine", 
+				"InputCore", 
+				"ModularGameplay",
+				"GameFeatures"
+			});
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[]
+		{
+			"EnhancedInput",
+			"CommonUI"
+		});
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
