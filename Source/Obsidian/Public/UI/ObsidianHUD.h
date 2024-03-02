30cf7ff49ac95f751d6c56876e5f655faf70495d
Michał Ogiński <michaloginski15@gmail.com>
2024-03-02 13:36:18 +0100

    Changed the git ignore

---
+++ Source/Obsidian/Public/UI/ObsidianHUD.h
@@ -0,0 +1,17 @@
+// Copyright 2024 Michał Ogiński
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "ObsidianHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OBSIDIAN_API AObsidianHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
