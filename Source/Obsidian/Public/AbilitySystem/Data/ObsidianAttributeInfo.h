6c0493487baf96b94edf669d03974ff4ec1a5d91
Michał <michaloginski15@gmail.com>
2024-05-13 19:58:34 +0200

    Created and populated the attribute info data asset

---
+++ Source/Obsidian/Public/AbilitySystem/Data/ObsidianAttributeInfo.h
@@ -0,0 +1,42 @@
+﻿// Copyright 2024 out of sCope team - Michał Ogiński
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Engine/DataAsset.h"
+#include "ObsidianAttributeInfo.generated.h"
+
+USTRUCT(BlueprintType)
+struct FOAttributeInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Attribute Tag")
+	FGameplayTag AttributeTag = FGameplayTag();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Attribute Name")
+	FText AttributeName = FText();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Attribute Description")
+	FText AttributeDescription = FText();
+
+	UPROPERTY(BlueprintReadOnly)
+	float AttributeValue = 0.f;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class OBSIDIAN_API UObsidianAttributeInfo : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	FOAttributeInfo FindAttributeInfoForTag(const FGameplayTag& AttributeTag, bool bLogIfNotFound = false) const;
+	
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Attribute Info")
+	TArray<FOAttributeInfo> AttributeInformation;
+};
