7c667401013605617908a258510450de9fcf213c
Michał <michaloginski15@gmail.com>
2024-09-10 23:08:25 +0200

    Created logging function to log vectors

--- Source/Obsidian/Private/Core/ObsidianBlueprintFunctionLibrary.cpp
+++ Source/Obsidian/Private/Core/ObsidianBlueprintFunctionLibrary.cpp
@@ -2,6 +2,9 @@
 
 
 #include "Core/ObsidianBlueprintFunctionLibrary.h"
+#include "Engine/Console.h"
+#include "Kismet/GameplayStatics.h"
+#include "Obsidian/Obsidian.h"
 
 bool UObsidianBlueprintFunctionLibrary::IsActorOfClass(const AActor* ActorToCheck, UClass* ActorClass)
 {
@@ -11,4 +14,36 @@ bool UObsidianBlueprintFunctionLibrary::IsActorOfClass(const AActor* ActorToChec
 	}
 	
 	return ActorToCheck->IsA(ActorClass);
+}
+
+void UObsidianBlueprintFunctionLibrary::PrintVector(const UObject* WorldContextObject, const FVector& VectorToLog, const FString& PrefixMessage,
+	const FName Key, const float TimeToDisplay, const FLinearColor MessageColor, const bool bPrintToScreen, const bool bPrintToLog)
+{
+	const FString FinalStringPrintMessage = FString::Printf(TEXT("[%s]: %f, %f, %f"), *PrefixMessage, VectorToLog.X, VectorToLog.Y, VectorToLog.Z);
+	
+	if(GAreScreenMessagesEnabled && bPrintToScreen && TimeToDisplay > 0.0f)
+	{
+		uint64 InnerKey = -1;
+		if(Key != NAME_None)
+		{
+			InnerKey = GetTypeHash(Key);
+		}
+		GEngine->AddOnScreenDebugMessage(InnerKey, TimeToDisplay, MessageColor.ToFColor(true), FinalStringPrintMessage);
+	}
+
+	if (bPrintToLog)
+	{
+		UE_LOG(LogObsidian, Log, TEXT("%s"), *FinalStringPrintMessage);
+		
+		APlayerController* PC = (WorldContextObject ? UGameplayStatics::GetPlayerController(WorldContextObject, 0) : nullptr);
+		ULocalPlayer* LocalPlayer = (PC ? Cast<ULocalPlayer>(PC->Player) : nullptr);
+		if (LocalPlayer && LocalPlayer->ViewportClient && LocalPlayer->ViewportClient->ViewportConsole)
+		{
+			LocalPlayer->ViewportClient->ViewportConsole->OutputText(FinalStringPrintMessage);
+		}
+	}
+	else
+	{
+		UE_LOG(LogObsidian, Verbose, TEXT("%s"), *FinalStringPrintMessage);
+	}
 }
--- Source/Obsidian/Public/Core/ObsidianBlueprintFunctionLibrary.h
+++ Source/Obsidian/Public/Core/ObsidianBlueprintFunctionLibrary.h
@@ -16,4 +16,8 @@ class OBSIDIAN_API UObsidianBlueprintFunctionLibrary : public UBlueprintFunction
 
 	UFUNCTION(BlueprintPure, Category = "ObsidianBlueprintFunctionLibrary")
 	static bool IsActorOfClass(const AActor* ActorToCheck, UClass* ActorClass);
+
+	UFUNCTION(BlueprintCallable, meta=(WorldContext="WorldContextObject", CallableWithoutWorldContext, Keywords = "log print", AdvancedDisplay = "3", DevelopmentOnly), Category = "ObsidianBlueprintFunctionLibrary")
+	static void PrintVector(const UObject* WorldContextObject, const FVector& VectorToLog, const FString& PrefixMessage = FString("Vector"), const FName Key = NAME_None,
+		const float TimeToDisplay = 3.0f, const FLinearColor MessageColor = FLinearColor::Green, const bool bPrintToScreen = true, const bool bPrintToLog = true);
 };
