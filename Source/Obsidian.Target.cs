3654731c77d8718f05618fcae78b8d7f25a8ad8a
Michał <michaloginski15@gmail.com>
2024-11-14 20:20:41 +0100

    Changed Include Order to Latest

--- Source/Obsidian.Target.cs
+++ Source/Obsidian.Target.cs
@@ -9,7 +9,7 @@ public class ObsidianTarget : TargetRules
 	{
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V4;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("Obsidian");
 	}
 }
