d96789d874f0a5c52495edc66d249a484dac7d8c
Michał <michaloginski15@gmail.com>
2025-05-03 20:32:51 +0200

    Sort of character creation screen

--- Source/Obsidian/Public/UI/FrontEnd/ObsidianCharacterCreationScreen.h
+++ Source/Obsidian/Public/UI/FrontEnd/ObsidianCharacterCreationScreen.h
@@ -6,6 +6,10 @@
 #include "UI/ObsidianActivatableWidget.h"
 #include "ObsidianCharacterCreationScreen.generated.h"
 
+class UEditableTextBox;
+class UCheckBox;
+class UObsidianButtonBase;
+class USizeBox;
 class UCommonTextBlock;
 /**
  * 
@@ -22,6 +26,30 @@ public:
 protected:
 	UPROPERTY(meta=(BindWidget))
 	TObjectPtr<UCommonTextBlock> TabName_TextBlock;
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<USizeBox> CharacterDetails_SizeBox;
+	
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UEditableTextBox> CharacterName_EditableTextBox;
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UCheckBox> Hardcore_CheckBox;
+	
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<USizeBox> CharacterList_SizeBox;
+	
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UObsidianButtonBase> Witch_Button;
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UObsidianButtonBase> Paladin_Button;
+	
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UObsidianButtonBase> Barbarian_Button;
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UObsidianButtonBase> Assassin_Button;
 	
 protected:
 	bool bIsOnlineCharacter = false;
